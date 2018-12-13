6758f851aaa6a7be28c29fbe2ff85c2a49b7c78b
CSuksudprasert <blackstones5054@gmail.com>
2018-12-13 23:32:14 +0700

    1

--- app/src/main/java/com/example/biim/appp/DatabaseHelper.java
+++ app/src/main/java/com/example/biim/appp/DatabaseHelper.java
@@ -1,4 +1,32 @@
 package com.example.biim.appp;
 
-class DatabaseHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "subject.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public static final String TABLE_NAME = "Subject";
+    public static final String COL_SUB = "";
+    public static final String COL_TITLE = "title";
+    public static final String COL_NUMBER = "number";
+    public static final String COL_IMAGE = "image";
+
+    public DatabaseHelper(@RecentlyNullable Context context, @RecentlyNullable String name, @RecentlyNullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
--- app/src/main/java/com/example/biim/appp/Main2Activity.java
+++ app/src/main/java/com/example/biim/appp/Main2Activity.java
@@ -2,12 +2,39 @@ package com.example.biim.appp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class Main2Activity extends AppCompatActivity {
+
+    Spinner daySpin;
+    Spinner timeStartSpin;
+    Spinner timeFinishSpin;
+
+    String arrday[] = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+    String arrtimeS[] = {"08.30","09.25","10.20","11.15","13.00","13.55","14.50","15.45","16.30","17.35","18.30","19.25"};
+    String arrtimeF[] = {"09.20","10.15","11.10","12.05","13.50","14.45","15.40","16.35","17.30","18.25","19.20","20.15"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        daySpin =  (Spinner) findViewById(R.id.day_spinner);
+        timeStartSpin =  (Spinner) findViewById(R.id.timeStart_spinner);
+        timeFinishSpin =  (Spinner) findViewById(R.id.timeFinish_spinner);
+
+        ArrayAdapter<String> adapterDay = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, arrday);
+        daySpin.setAdapter(adapterDay);
+
+        ArrayAdapter<String> adapterTimeS = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, arrtimeS);
+        timeStartSpin.setAdapter(adapterTimeS);
+
+        ArrayAdapter<String> adapterTimeF = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, arrtimeF);
+        timeFinishSpin.setAdapter(adapterTimeF);
     }
 }
