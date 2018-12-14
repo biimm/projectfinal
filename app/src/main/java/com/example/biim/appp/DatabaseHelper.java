33b142e2c68faca61da77fbb3a1848402fca56e0
CSuksudprasert <blackstones5054@gmail.com>
2018-12-14 15:42:37 +0700

    commit 4

--- app/src/main/java/com/example/biim/appp/DatabaseHelper.java
+++ app/src/main/java/com/example/biim/appp/DatabaseHelper.java
@@ -11,17 +11,20 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
 
     public static final String TABLE_NAME = "Schedule";
+    public static final String COL_ID = "_id";
     public static final String COL_SUB = "Subject";
-    public static final String COL_DAY = "title";
-    public static final String COL_TIMESTART = "number";
-    public static final String COL_TIMEFINISH = "image";
+    public static final String COL_DAY = "Day";
+    public static final String COL_TIME = "Time";
+    public static final String COL_CLASS = "Classroom";
+
 
     private static final String SQL_CREATE_TABLE_SCHEDULE
             = "CREATE TABLE " + TABLE_NAME + "("
-            + COL_SUB + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+            + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+            + COL_SUB + " TEXT,"
             + COL_DAY + " TEXT,"
-            + COL_TIMESTART + " TEXT,"
-            + COL_TIMEFINISH + " TEXT "
+            + COL_TIME + " TEXT,"
+            + COL_CLASS + "TEXT"
             + ")";
 
     public DatabaseHelper(Context context) { super(context,"Schedule.db",null,1); }
@@ -32,11 +35,45 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL(SQL_CREATE_TABLE_SCHEDULE);
 
         ContentValues cv = new ContentValues();
+        cv.put(COL_SUB,"ภาษาไทย");
+        cv.put(COL_DAY, "Monday");
+        cv.put(COL_TIME,"08.30-10.15");
+        cv.put(COL_CLASS,"1240");
+        db.insert(TABLE_NAME,null,cv);
+
+        cv = new ContentValues();
+        cv.put(COL_SUB,"ภาษาไทย");
+        cv.put(COL_DAY, "Tuesday");
+        cv.put(COL_TIME,"");
+        cv.put(COL_CLASS,);
+        db.insert(TABLE_NAME,null,cv);
+
+        cv = new ContentValues();
+        cv.put(COL_SUB,"ภาษาไทย");
+        cv.put(COL_DAY, "Wednesday");
+        cv.put(COL_TIME,"");
+        cv.put(COL_CLASS,);
+        db.insert(TABLE_NAME,null,cv);
+
+        cv = new ContentValues();
+        cv.put(COL_SUB,"ภาษาไทย");
+        cv.put(COL_DAY, "Thursday");
+        cv.put(COL_TIME,"");
+        cv.put(COL_CLASS,);
+        db.insert(TABLE_NAME,null,cv);
+
+        cv = new ContentValues();
+        cv.put(COL_SUB,"ภาษาไทย");
+        cv.put(COL_DAY, "Friday");
+        cv.put(COL_TIME,"");
+        cv.put(COL_CLASS,);
+        db.insert(TABLE_NAME,null,cv);
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
 
     }
 }
--- app/src/main/java/com/example/biim/appp/Main2Activity.java
+++ app/src/main/java/com/example/biim/appp/Main2Activity.java
@@ -9,12 +9,11 @@ import android.widget.Spinner;
 public class Main2Activity extends AppCompatActivity {
 
     Spinner daySpin;
-    Spinner timeStartSpin;
-    Spinner timeFinishSpin;
+    Spinner timeSpin;
 
-    String arrday[] = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
-    String arrtimeS[] = {"08.30","09.25","10.20","11.15","13.00","13.55","14.50","15.45","16.30","17.35","18.30","19.25"};
-    String arrtimeF[] = {"09.20","10.15","11.10","12.05","13.50","14.45","15.40","16.35","17.30","18.25","19.20","20.15"};
+
+    String arrday[] = {"Monday","Tuesday","Wednesday","Thursday","Friday"};
+    String arrtimeS[] = {"08.30-10.15","10.20-12.05","13.00-14.45","14.50-16.35"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +21,7 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         daySpin =  (Spinner) findViewById(R.id.day_spinner);
-        timeStartSpin =  (Spinner) findViewById(R.id.timeStart_spinner);
-        timeFinishSpin =  (Spinner) findViewById(R.id.timeFinish_spinner);
+        timeSpin =  (Spinner) findViewById(R.id.timeStart_spinner);
 
         ArrayAdapter<String> adapterDay = new ArrayAdapter<String>(this,
                 android.R.layout.simple_dropdown_item_1line, arrday);
@@ -31,10 +29,9 @@ public class Main2Activity extends AppCompatActivity {
 
         ArrayAdapter<String> adapterTimeS = new ArrayAdapter<String>(this,
                 android.R.layout.simple_dropdown_item_1line, arrtimeS);
-        timeStartSpin.setAdapter(adapterTimeS);
+        timeSpin.setAdapter(adapterTimeS);
 
-        ArrayAdapter<String> adapterTimeF = new ArrayAdapter<String>(this,
-                android.R.layout.simple_dropdown_item_1line, arrtimeF);
-        timeFinishSpin.setAdapter(adapterTimeF);
+
+
     }
 }
